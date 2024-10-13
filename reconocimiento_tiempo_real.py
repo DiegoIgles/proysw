0a257fecd492ede26dbeabeddef60646d3272f42
AlexRomanR <thealex6969@gmail.com>
2024-10-13 03:59:53 -0400

    anomalias xd

--- reconocimiento_tiempo_real.py
+++ reconocimiento_tiempo_real.py
@@ -9,28 +9,11 @@ modelo_objeto = tf.keras.models.load_model('modelo_objeto.h5')
 # Cargar el modelo de detección de anomalías sin compilar y luego compilar
 autoencoder = tf.keras.models.load_model('autoencoder_model.h5', compile=False)
 autoencoder.compile(optimizer='adam', loss='mse')
-                                              #estos dos se pueden modificar a gusto
-def ajustar_umbral_anomalia(prediction_value, min_threshold=0.001, max_threshold=0.005):
-    """
-    Ajusta el umbral de anomalía basado en el valor de predicción.
-
-    :param prediction_value: Valor de predicción del modelo de detección de objetos (0 a 1).
-    :param min_threshold: Umbral mínimo de anomalía.
-    :param max_threshold: Umbral máximo de anomalía.
-    :return: Umbral de anomalía ajustado.
-    """
-    # Asegurar que prediction_value esté entre 0 y 1
-    prediction_value = np.clip(prediction_value, 0, 1)
-    
-    # Calcular el umbral de anomalía directamente proporcional al prediction_value (lo que está en parentesis se puede modificar a gusto)
-    anomaly_threshold = min_threshold + ((prediction_value-(prediction_value*0.01)) * (max_threshold - (min_threshold+0.0005)))
-    
-    return anomaly_threshold
 
 def real_time_detection():
     cap = cv2.VideoCapture(0)
-    base_anomaly_threshold = 0.005  # Umbral base para anomalías
-    base_object_threshold = 0.5       # Umbral base para detección de objeto, se puede modificar a gusto
+    anomaly_threshold = 0.0107  # AJUSTAR ESTE VALOR A UN VALOR CERCANO AL QUE APARECE CUANDO EJECUTAS EL COMANDO DE RECONOCIMIENTO EN TIEMPO REAL, UN VALOR CERCANO AL ERROR DE RECONSTRUCCION
+    object_threshold = 0.2    # Umbral para detección de objeto
 
     while True:
         ret, frame = cap.read()
@@ -47,10 +30,6 @@ def real_time_detection():
         prediction_value = prediction[0][0]
         print(f"Predicción de objeto: {prediction_value}")
 
-        # Ajustar el umbral de anomalía basado en prediction_value
-        anomaly_threshold = ajustar_umbral_anomalia(prediction_value)
-        print(f"Umbral de anomalía ajustado: {anomaly_threshold}")
-
         # Predicción para detección de anomalías
         reconstructed = autoencoder.predict(img_norm_expanded)
         error = np.mean(np.square(img_norm_expanded - reconstructed))
@@ -58,7 +37,7 @@ def real_time_detection():
 
         # Decidir etiquetas y colores basados en las predicciones
         # Detección de objeto
-        if prediction_value > base_object_threshold:
+        if prediction_value > object_threshold:
             obj_label = "Objeto detectado"
             obj_color = (0, 255, 0)  # Verde
             # Dibujar rectángulo alrededor del objeto (ajusta las coordenadas según sea necesario)
